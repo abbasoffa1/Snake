660adc60b8f0f205b1c31281840c808a1a78fcdf
Gunay <gunayaa@code.edu.az>
2018-03-11 17:39:43 +0400

    ilan

---
+++ Ilan/Ilan/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Ilan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn = new System.Windows.Forms.Button();
+            this.bt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.AllowDrop = true;
+            this.btn.BackColor = System.Drawing.SystemColors.Control;
+            this.btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn.Image = ((System.Drawing.Image)(resources.GetObject("btn.Image")));
+            this.btn.Location = new System.Drawing.Point(12, 12);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(183, 62);
+            this.btn.TabIndex = 0;
+            this.btn.UseVisualStyleBackColor = false;
+            this.btn.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // bt
+            // 
+            this.bt.AllowDrop = true;
+            this.bt.BackColor = System.Drawing.Color.Maroon;
+            this.bt.Location = new System.Drawing.Point(298, 234);
+            this.bt.Name = "bt";
+            this.bt.Size = new System.Drawing.Size(55, 15);
+            this.bt.TabIndex = 1;
+            this.bt.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.bt);
+            this.Controls.Add(this.btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Button bt;
+    }
+}
+
---
+++ Ilan/Ilan/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ilan
+{
+    public partial class Form1 : Form
+    {
+        int currentLeft;
+        int currentTop;
+        int size = 30;
+        List<int> kordinat = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btn.Width = size;
+            btn.Height = size;
+            btn.Left = currentLeft;
+            btn.Top = currentTop;
+            bt.Width = size;
+            bt.Height = size;
+            
+            int list_count = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                kordinat.Add(list_count);
+                list_count += size;
+            }
+            Create_Eat();
+        }
+        
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {           
+            if (e.KeyCode == Keys.W)
+            {
+                if (btn.Location.X.ToString() == bt.Location.X.ToString() && btn.Location.Y.ToString() == bt.Location.Y.ToString())
+                {
+                    btn.Width = btn.Width += size / 2;
+                    btn.Height = btn.Height += size / 2;
+                    Create_Eat();    
+                }
+              
+                currentTop -= 30;
+                btn.Top = currentTop;
+                if ((btn.Top < 0) || (this.Width == btn.Top) || (this.Width - 36 < btn.Top))
+                {
+                    MessageBox.Show("Game Over");
+                }
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                if (btn.Location.X.ToString() == bt.Location.X.ToString() && btn.Location.Y.ToString() == bt.Location.Y.ToString())
+                {
+                    btn.Width = btn.Width += size / 2;
+                    btn.Height = btn.Height += size / 2;
+                    Create_Eat();
+                }
+               
+                currentTop += 30;
+                btn.Top = currentTop;
+                if ((btn.Top < 0) || (this.Width == btn.Top) || (this.Width - 36 < btn.Top))
+                {
+                    MessageBox.Show("Game Over");
+                }
+            }
+            else if (e.KeyCode == Keys.A)
+            {
+                if (btn.Location.X.ToString() == bt.Location.X.ToString() && btn.Location.Y.ToString() == bt.Location.Y.ToString())
+                {
+                    btn.Width = btn.Width += size / 2;
+                    btn.Height = btn.Height += size / 2;
+                    Create_Eat();
+                }
+               
+                currentLeft -= 30;
+                btn.Left = currentLeft;
+                if ((btn.Left < 0) || (this.Width == btn.Left) || (this.Width - 36 < btn.Left))
+                {
+                    MessageBox.Show("Game Over");
+                }
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                if (btn.Location.X.ToString() == bt.Location.X.ToString() && btn.Location.Y.ToString() == bt.Location.Y.ToString())
+                {
+                    btn.Width = btn.Width += size / 2;
+                    btn.Height = btn.Height += size / 2;
+                    Create_Eat();
+                }
+               
+                currentLeft += 30;
+                btn.Left = currentLeft;
+                if ((btn.Left < 0) || (this.Width == btn.Left) || (this.Width - 36 < btn.Left))
+                {
+                    MessageBox.Show("Game Over");
+                }
+            }
+        }
+        private void Create_Eat()
+        {
+            Random rand = new Random();
+            bt.Left = kordinat[rand.Next(0, kordinat.Count)];
+            bt.Top = kordinat[rand.Next(0, kordinat.Count)];
+
+        }
+        private void Uzanmag()
+        {
+            var SnakeBody = new SnakeBody();
+            
+        }
+        
+    }
+    public class SnakeBody
+    {
+        public string Tail;
+        public string Head;
+        public SnakeBody()
+        {
+           
+        }
+    }
+}
